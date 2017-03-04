839d88e4187d8ca67b400da957beff27d388e62a
YuriiKrat <yuriikrat97@gmail.com>
2017-03-05 00:33:17 +0200

    android project added

---
+++ android/app/src/main/java/com/gmail/yuriikrat/friendtreasury/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.yuriikrat.friendtreasury;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
